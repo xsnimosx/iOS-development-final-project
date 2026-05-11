539ad2bea24fee71e4f530297823c89557d92074
snimos <snimos@snimoss-Mac.local>
2026-05-11 23:33:38 +0800

    feat: add Firebase SDK and initialize

--- Chatapp/Chatapp/AppDelegate.swift
+++ Chatapp/Chatapp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        FirebaseApp.configure()
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
