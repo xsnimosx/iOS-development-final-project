29507a866c411bf9903b0dcabaf021275c8aa869
snimos <snimos@snimoss-Mac.local>
2026-05-11 20:59:46 +0800

    init: create Xcode project

---
+++ Chatapp/Chatapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatapp
+//
+//  Created by snimos on 2026/5/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
