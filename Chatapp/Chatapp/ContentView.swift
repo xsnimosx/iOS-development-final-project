ceaa11cd9ef58f67a6fd065a301abd7ab08e709f
MasonChrispher <140596585+MasonChrispher@users.noreply.github.com>
2026-06-10 10:03:15 +0800

    hell

---
+++ Chatapp/Chatapp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Chatapp
+//
+//  Created by  serene on 2026/6/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
